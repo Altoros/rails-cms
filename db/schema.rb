c67002342f59ed20491916745d12a9ba1a7721b3
lcostantini <costantinileandro1@gmail.com>
2017-03-15 13:35:30 -0300

    Add price column to images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314184117) do
+ActiveRecord::Schema.define(version: 20170315163201) do
 
   create_table "images", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170314184117) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.integer  "price"
   end
 
   create_table "pictures", force: :cascade do |t|
