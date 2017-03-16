80af8cd461614c646ac153a78ba2837d44caaa2f
lcostantini <costantinileandro1@gmail.com>
2017-03-16 11:13:51 -0300

    Add charges controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/cms', as: 'rails_admin'
   resources :images
+  resources :charges
   devise_for :users
   root to: 'rails_admin/main#dashboard'
 end
