4785519014c7b24f126f990db6b386983ad2185d
lcostantini <costantinileandro1@gmail.com>
2017-03-16 16:06:17 -0300

    Add welcome controller and view

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:name, :tags, :photo)
+      params.require(:image).permit(:name, :tags, :photo, :price)
     end
 end
