c2a62cbd6ac618045ad6a5007d476b55d52c5b31
lcostantini <costantinileandro1@gmail.com>
2017-03-21 13:23:12 -0300

    Show all the videos in welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    client = JWPlayer::API::Client.new
+    url = client.signed_url('videos/list')
+    @videos = HTTParty.get(url)["videos"]
     @images = Image.to_shop(current_user)
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,6 @@
 module WelcomeHelper
+  def jwplayer_video_tag video_id
+    src = "//content.jwplatform.com/players/#{video_id}-#{ENV["JWPLAYER_VIDEO_ID"]}.js"
+    "<script src=#{src}></script>"
+  end
 end
